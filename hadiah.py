0909f11a44ef45e3f42beb25d49c9b812f93b497
xchweetz <134176369+rendibau@users.noreply.github.com>
2023-07-26 19:52:08 +0700

    Update hadiah.py

--- hadiah.py
+++ hadiah.py
@@ -1,4 +1,4 @@
-print("----------------- Program Kasir Sederhana Ekorkode -----------------")
+print("----------------- Program Kasir Sederhana -----------------")
 pembeli = input("Masukkan nama Pembeli: ")
 print ("Nama Pembeli :", pembeli) 
 
